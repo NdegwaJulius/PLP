29907f6d867a8292daa7c794eaf08ca87930cc4c
NdegwaJulius <juliusndegwa2@gmail.com>
2024-02-27 20:18:48 +0100

    cleaner

--- control.dart
+++ control.dart
@@ -1,22 +1,13 @@
 import 'dart:io';
 
 void main() {
-  // Prompting the user for input
   stdout.write("Enter a number: ");
-  String input = stdin.readLineSync()!;
-  
-  // Converting the input to an integer
   try {
-    int number = int.parse(input);
+    int number = int.parse(stdin.readLineSync()!);
 
-    // Checking the conditions and printing the appropriate message
-    if (number > 10) {
-      print("Your number is greater than 10");
-    } else if (number < 10) {
-      print("Your number is less than 10");
-    } else {
-      print("Your number is equal to 10");
-    }
+    print(number > 10 ? "Your number is greater than 10"
+        : number < 10 ? "Your number is less than 10"
+        : "Your number is equal to 10");
   } catch (e) {
     print("Invalid input. Please enter a valid number.");
   }
